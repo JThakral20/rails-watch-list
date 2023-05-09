f9e78c1643c387450adac3d02ce824f23c3eb348
Jasmine Thakral <jasminethakral@gmail.com>
2023-05-09 20:16:56 +0100

    second challenge additions

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
-  has_many :movies
   validates :name, presence: true, uniqueness: true
   has_many :movies, through: :bookmarks
+  has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "lists#index"
+  # root to: "lists#index"
+  resources :lists, only: [:create, :index, :new, :show] do
+    resources :bookmarks, only: [:create, :new ]
+    resources :reviews, only: :create
+    # resources :movies, only: [:index]
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
+
+#have to have the nesting with the movies as well?
+# make movie controller for just index
+# controller just normal - knows bookmarks are accessed from the list as its nested
